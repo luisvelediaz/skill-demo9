69b049e77ef8fcd67395064b8df912bdacc4b6b3
luisvelediaz <103283619+luisvelediaz@users.noreply.github.com>
2022-04-27 15:38:22 -0700

    test file that fails

--- skillDemo.java
+++ skillDemo.java
@@ -1,3 +1,6 @@
 public class skillDemo{
-//
+
+    public static int subtract(int n, int m){
+        return n-m;
+    }
 }
---
+++ skillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillDemoTest {
+
+// J unit Test 
+    @Test
+      public void subtraction() {
+        assertEquals(2, skillDemo.subtract(2 , 1)); //Test fails Opearation  
+       }
+}
